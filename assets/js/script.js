08f5f751fa8db9929c16cd87f440e6df5944acab
Jordan Phillipps <Jordan1@jordans-mbp.home>
2017-10-05 18:28:35 +0100

    temporary animations

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,80 +1,3 @@
-/** var lFollowX = 0,
-    lFollowY = 0,
-    x = 0,
-    y = 0,
-    friction = 1 / 30;
-
-function moveBackground() {
-  x += (lFollowX - x) * friction;
-  y += (lFollowY - y) * friction;
-  
-  translate = 'translate(' + x + 'px, ' + y + 'px) scale(1.1)';
-
-  $('.design svg').css({
-    '-webit-transform': translate,
-    '-moz-transform': translate,
-    'transform': translate
-  });
-
-  $('.hero-overlay .circle').css({
-    '-webit-transform': translate,
-    '-moz-transform': translate,
-    'transform': translate,
-    
-  });
-  window.requestAnimationFrame(moveBackground);
-}
-
-$(window).on('mousemove click', function(e) {
-
-  var lMouseX = Math.max(-100, Math.min(100, $(window).width() / 2 - e.clientX));
-  var lMouseY = Math.max(-100, Math.min(100, $(window).height() / 2 - e.clientY));
-  lFollowX = (20 * lMouseX) / 100; // 100 : 12 = lMouxeX : lFollow
-  lFollowY = (10 * lMouseY) / 100;
-
-});
-
-moveBackground();
-
-var lFollowXTwo = 0,
-    lFollowYTwo = 0,
-    b = 0,
-    c = 0,
-    frict = 1 / 100;
-
-function moveText() {
-  b += (lFollowXTwo - b) * frict;
-  c += (lFollowYTwo - c) * frict;
-  
-  translate = 'translate(' + b + 'px, ' + c + 'px) scale(1)';
-
-  $('.hero-overlay .text h1 ').css({
-    '-webit-transform': translate,
-    '-moz-transform': translate,
-    'transform': translate
-  });
-
-  $('.hero-overlay .text h2 ').css({
-    '-webit-transform': translate,
-    '-moz-transform': translate,
-    'transform': translate
-  });
-  window.requestAnimationFrame(moveText);
-}
-
-$(window).on('mousemove click', function(e) {
-
-  var lMouseX = Math.max(-100, Math.min(100, $(window).width() / 2 - e.clientX));
-  var lMouseY = Math.max(-100, Math.min(100, $(window).height() / 2 - e.clientY));
-  lFollowXTwo = (20 * lMouseX) / 100; // 100 : 12 = lMouxeX : lFollow
-  lFollowYTwo = (10 * lMouseY) / 100;
-
-});
-
-moveText();**/
-
-/** Logo scroll **/
-
 $(window).bind('scroll', function() {
      if ($(window).scrollTop() > 30) {
          $('.logo').addClass('shrink');
@@ -83,6 +6,44 @@ $(window).bind('scroll', function() {
          $('.logo').removeClass('shrink');
      }
 });
+
+
+/** POrtfolio **/
+
+$(window).scroll(function(){
+  var pScroll = $(this).scrollTop();
+
+  $('.bg-port').css({
+    'transform' : 'translate(0px, -'+ pScroll /12 +'%)'
+  });
+});
+
+$(window).scroll(function(){
+  var pScroll = $(this).scrollTop();
+
+  $('.bg-about').css({
+    'transform' : 'translate(0px, -'+ pScroll /12 +'%)'
+  });
+});
+
+$(document).ready(function() {
+    $('#projectGr div').each(function(i){
+
+          setTimeout(function(){
+              $('#projectGr div').eq(i).addClass('is-showing');
+          }, 50 * (i+1));
+      });
+
+      $('.portfolio #mainTitle').addClass('move-in');
+
+      $('.about-me .text').addClass('show');
+
+      $('.about-me .figure').addClass('show');
+}); 
+
+
+
+/* Home Animations */
 
 $('.js-tilt').tilt({
   scale: 1.1
@@ -122,4 +83,6 @@ $hmSub
     {translateY: "0px"}, 
     {delay:900}
     );
+
+
 
